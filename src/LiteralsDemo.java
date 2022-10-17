28bc256a74a111c3ca416f0a77a9f1e160ee1f37
djpn <36060426+davidjpnelson@users.noreply.github.com>
2022-10-17 10:45:28 -0500

    char literals and String literals

--- src/LiteralsDemo.java
+++ src/LiteralsDemo.java
@@ -1,17 +1,28 @@
 public class LiteralsDemo {
 
   public static void main(String[] args) {
+    char sq = '\'';
+    char dq ='\"';
+    char bksl = '\\';
+    char cr = '\r';
+    char nl = '\n';
+    char ff = '\f';
+    char ht = '\t';
+    char bs = '\b';
+    char octalI = '\111';
+    char hexay = '\u00FF';
+
     System.out.println("First Line\nSecond Line");
-    System.out.println("Single Quote: '\n"
-        + "Double Quote: \"\n"
-        + "Backslash: \\\n"
-        + "Carriage Return\n here\n"
-        + "New Line: \n see?\n"
-        + "Form Feed:\ffeed\n"
-        + "Horizontal tab: \tA\tB\n"
-        + "Backspace: \b spaced back\n"
-        + "Octal 111 \111\n"
-        + "Hexa 00FF: \u00FF");
+    System.out.println("Single Quote: " + sq + "\n"
+        + "Double Quote: " + dq +"\n"
+        + "Backslash: " + bksl + "\n"
+        + "Carriage Return" + cr +"see\n"
+        + "New Line: " + nl + "see?\n"
+        + "Form Feed:" + ff + "feed\n"
+        + "Horizontal tab:" + ht + "see?\n"
+        + "Backspace: " + bs +"spaced back\n"
+        + "Octal 111: " + octalI + "\n"
+        + "Hexa 00FF: " + hexay + "\n");
     System.out.println("Tab Demo\nA\tB\tC\nD\tE\tF");
 
   }
