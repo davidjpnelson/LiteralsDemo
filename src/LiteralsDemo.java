18c9346a15c677a7550735009c556073af2d1cb9
djpn <36060426+davidjpnelson@users.noreply.github.com>
2022-10-17 10:16:18 -0500

    demo of escapes for literals

--- src/LiteralsDemo.java
+++ src/LiteralsDemo.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class LiteralsDemo {
+public class LiteralsDemo {
+
+  public static void main(String[] args) {
+    System.out.println("First Line\nSecond Line");
+    System.out.println("Single Quote: '\n"
+        + "Double Quote: \"\n"
+        + "Backslash: \\\n"
+        + "Carriage Return\n here\n"
+        + "New Line: \n see?\n"
+        + "Form Feed:\ffeed\n"
+        + "Horizontal tab: \tA\tB\n"
+        + "Backspace: \b spaced back\n"
+        + "Octal 111 \111\n"
+        + "Hexa 00FF: \u00FF");
+    System.out.println("Tab Demo\nA\tB\tC\nD\tE\tF");
+
+  }
 }
